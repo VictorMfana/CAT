b10613dcb212dd72dee79f95e41ace07790374c8
ErykaN <105892689+ErykaN@users.noreply.github.com>
2023-11-30 14:01:03 +0300

    Add files via upload

---
+++ indexgame.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Shape Escape</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Shape Escape</h1>
+        <p>This is the index page.</p>
+        
+        <a href="login.html">Login</a>
+        <a href="register.html">Register</a>
+    </div>
+</body>
+</html>
