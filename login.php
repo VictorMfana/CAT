4d9135c876e37c9f4b28a29019a011ce290f0691
ErykaN <105892689+ErykaN@users.noreply.github.com>
2023-11-30 14:01:36 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+// Include the database connection file
+include('db_connection.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Login</h1>
+        <form method="post" action="login_process.php">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit" name="login">Login</button>
+        </form>
+        
+        <a href="index.php">Go to Home</a>
+        <a href="register.php">Register</a>
+    </div>
+</body>
+</html>
